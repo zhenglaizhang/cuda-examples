a762769b97bd425f0817143ea5f01bf4f0ae94b2
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2018-01-10 11:53:05 +0800

    fix add_grid

--- add_grid.cu
+++ add_grid.cu
@@ -1,25 +1,24 @@
 #include <iostream>
 #include <math.h>
+
 // Kernel function to add the elements of two arrays
 __global__
-void add(int n, float *x, float *y)
-{
+void add(int n, float *x, float *y) {
     // threadIdx.x contains the index of the current thread within its block,
     // blockDim.x contains the number of threads in the block
-    int index = threadIdx.x;
-    int stride = blockDim.x;
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
     for (int i = index; i < n; i += stride)
         y[i] = x[i] + y[i];
 }
 
-int main(void)
-{
-    int N = 1<<20;
+int main(void) {
+    int N = 1 << 20;
     float *x, *y;
 
     // Allocate Unified Memory – accessible from CPU or GPU
-    cudaMallocManaged(&x, N*sizeof(float));
-    cudaMallocManaged(&y, N*sizeof(float));
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
 
     // initialize x and y arrays on the host
     for (int i = 0; i < N; i++) {
@@ -32,7 +31,7 @@ int main(void)
     // CUDA GPUs run kernels using blocks of threads that are a multiple of 32 in size
     int blockSize = 256;
     int numBlocks = (N + blockSize - 1) / blockSize;
-    add<<<numBlocks, blockSize>>>(N, x, y);
+    add << < numBlocks, blockSize >> > (N, x, y);
 
     // Wait for GPU to finish before accessing on host
     cudaDeviceSynchronize();
@@ -40,7 +39,7 @@ int main(void)
     // Check for errors (all values should be 3.0f)
     float maxError = 0.0f;
     for (int i = 0; i < N; i++)
-        maxError = fmax(maxError, fabs(y[i]-3.0f));
+        maxError = fmax(maxError, fabs(y[i] - 3.0f));
     std::cout << "Max error: " << maxError << std::endl;
 
     // Free memory
